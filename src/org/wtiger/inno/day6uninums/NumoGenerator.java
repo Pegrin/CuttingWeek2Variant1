055e295c6ec7aae8a0b4fdb166e3b2c881b00b81
Pegrin <mail@wtiger.org>
2017-02-13 10:04:54 +0300

    update

--- src/org/wtiger/inno/day6uninums/NumoGenerator.java
+++ src/org/wtiger/inno/day6uninums/NumoGenerator.java
@@ -14,7 +14,7 @@ public class NumoGenerator implements Runnable {
     public void run() {
         while (!set.stopped()) {
             try {
-                Thread.sleep(500);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
